17f374604f7f6cd8d5b9ffd4566a6b15ae25378a
Denchik90 <Denchik90@.github.com>
2019-04-17 16:25:56 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
+$(document).ready(function () {
+    $('.slider__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.timeline__block',
+        infinite: false
+    });
+    $('.timeline__block').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider__content',
+        centerMode: true,
+        focusOnSelect: true,
+        infinite: false
+    });
+    $('.timeline__block button').text('');
 
-$('.slider__content').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    asNavFor: '.timeline__block',
-    infinite: false
+    $('img').on('click', function () {
+        let img = $(this).attr('data-img');
+       if(typeof img !== typeof undefined && img !== false){
+            $('.modal').fadeIn();
+            $('.modal img').attr('src', img);
+       }else {
+           console.log("no")
+       }
+    });
+    $('.modal span').bind('click',function () {
+        $('.modal').fadeOut()
+    })
 });
-$('.timeline__block').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider__content',
-    centerMode: true,
-    focusOnSelect: true,
-    infinite: false
-
-});
-$('.timeline__block button').text('');
